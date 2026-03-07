d42cfcab753684b6311e0deff687fc8b300a01af
Nguyễn Thanh Sơn <sonnguyen5810@MacBook-Air-cua-Nguyen.local>
2026-03-07 15:16:33 +0700

    css locator string

--- tests/checkbox.spec.ts
+++ tests/checkbox.spec.ts
@@ -1,13 +1,6 @@
 import { test, expect } from '@playwright/test';
-
-test('checkbox', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/checkboxes');
-  await page.getByRole('checkbox').first().check();
-  await page.getByRole('checkbox').first().uncheck();
-  await page.getByRole('checkbox').nth(1).uncheck();
-  await page.getByRole('checkbox').nth(1).check();
-});
-
+// 
+// Role locators
 test('test', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/checkboxes');
   await page.getByRole('checkbox').first().check();
@@ -17,6 +10,8 @@ test('test', async ({ page }) => {
   await expect(page.locator('#checkboxes')).toContainText('checkbox 1 checkbox 2');
 });
 
+
+// CSS locators
 test('checkbox using CSS locators', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/checkboxes');
   await page.locator('#checkboxes').getByRole('checkbox').first().check();
@@ -26,10 +21,27 @@ test('checkbox using CSS locators', async ({ page }) => {
   await expect(page.locator('#checkboxes')).toContainText('checkbox 1 checkbox 2');
 });
 
+
+// Xpath locators
 test('checkbox using XPath locators', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/checkboxes');
   await page.locator('//input[@type]="checkbox")').fill('checkbox 1');
   await page.locator('//input[@type]="checkbox")').nth(1).uncheck();
   await page.locator('//input[@type]="checkbox")').nth(1).check();
   await expect(page.locator('//input[@type]="checkbox")')).toContainText('checkbox 1 checkbox 2');
+});
+
+// Xpath locators with attribute
+test('verify able to check the checkbox', async ({page}) =>{
+    await page.goto('https://the-internet.herokuapp.com/checkboxes')
+
+    await page.getByRole('checkbox').first().check(); // access by role locator
+    await page.locator("#checkboxes input:nth-child(1)").check(); //css locator string ==> unique locator
+    await page.locator("//*[@id='checkboxes']/input[1]").check(); //xpath
+    await page.locator("//*[@id='checkboxes']/input[1]").isChecked(); //xpath
+
+    expect(await page.getByRole('checkbox').first()).toBeChecked();
+
+    await page.getByRole('checkbox').nth(1).check();
+    expect(await page.getByRole('checkbox').nth(1)).toBeChecked();
 });
