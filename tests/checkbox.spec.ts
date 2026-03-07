aa4457b50d0c5efd399fb148ea76e3f32c20eeec
Nguyễn Thanh Sơn <sonnguyen5810@MacBook-Air-cua-Nguyen.local>
2026-03-07 14:03:21 +0700

    add notes

--- tests/checkbox.spec.ts
+++ tests/checkbox.spec.ts
@@ -7,3 +7,12 @@ test('checkbox', async ({ page }) => {
   await page.getByRole('checkbox').nth(1).uncheck();
   await page.getByRole('checkbox').nth(1).check();
 });
+
+test('test', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/checkboxes');
+  await page.getByRole('checkbox').first().check();
+  await page.getByRole('checkbox').first().uncheck();
+  await page.getByRole('checkbox').nth(1).uncheck();
+  await page.getByRole('checkbox').nth(1).check();
+  await expect(page.locator('#checkboxes')).toContainText('checkbox 1 checkbox 2');
+});
