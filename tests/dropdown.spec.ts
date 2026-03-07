62ce3435c5912dfd7e8d08f90b04682cef8a7f85
Nguyễn Thanh Sơn <sonnguyen5810@MacBook-Air-cua-Nguyen.local>
2026-03-07 16:09:43 +0700

    hyperlink.spec.ts

--- tests/dropdown.spec.ts
+++ tests/dropdown.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('verify dropdown', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/dropdown');
-    await page.locator('#dropdown').click();
-    await page.locator(`//select[@id='dropdown']`).selectOption('1');
+    await page.locator('#dropdown').selectOption({ label: 'Option 1' });
     await expect(page.locator('#dropdown')).toContainText('Option 1');
 });
