4c4d1e1d724699bea795ddc59f0d10f2852d1171
Nguyễn Thanh Sơn <sonnguyen5810@MacBook-Air-cua-Nguyen.local>
2026-03-07 15:04:01 +0700

    add test using xpath/css

--- tests/checkbox.spec.ts
+++ tests/checkbox.spec.ts
@@ -16,3 +16,20 @@ test('test', async ({ page }) => {
   await page.getByRole('checkbox').nth(1).check();
   await expect(page.locator('#checkboxes')).toContainText('checkbox 1 checkbox 2');
 });
+
+test('checkbox using CSS locators', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/checkboxes');
+  await page.locator('#checkboxes').getByRole('checkbox').first().check();
+  await page.locator('#checkboxes').getByRole('checkbox').first().uncheck();
+  await page.locator('#checkboxes').getByRole('checkbox').nth(1).uncheck();
+  await page.locator('#checkboxes').getByRole('checkbox').nth(1).check();
+  await expect(page.locator('#checkboxes')).toContainText('checkbox 1 checkbox 2');
+});
+
+test('checkbox using XPath locators', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/checkboxes');
+  await page.locator('//input[@type]="checkbox")').fill('checkbox 1');
+  await page.locator('//input[@type]="checkbox")').nth(1).uncheck();
+  await page.locator('//input[@type]="checkbox")').nth(1).check();
+  await expect(page.locator('//input[@type]="checkbox")')).toContainText('checkbox 1 checkbox 2');
+});
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,10 +2,26 @@ import { test, expect } from '@playwright/test';
 
 test('login success', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
-  
-  
   await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
   await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page.getByRole('heading', { name: 'Welcome to the Secure Area. When you are done click logout below.' })).toBeVisible();
+});
+
+test('login success using CSS locators', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.locator('#username').fill('tomsmith'); // 
+  await page.locator('#password').fill('SuperSecretPassword!');
+  await page.locator('button[type="submit"]').click();
+  await expect(page.locator('h2')).toHaveText('Secure Area');
+  await expect(page.locator('.subheader')).toContainText('Welcome to the Secure Area');
+});
+
+test('login success using XPath locators', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.locator('//input[@id="username"]').fill('tomsmith'); //tìm tag + attribute
+  await page.locator('//*[@id="password"]').fill('SuperSecretPassword!'); // tìm tất cả các tag có attribute id = password
+  await page.locator('//button[@type="submit"]').click();
+  await expect(page.locator('//h2')).toHaveText('Secure Area');
+  await expect(page.locator('//h4[@class="subheader"]')).toContainText('Welcome to the Secure Area');
 });
